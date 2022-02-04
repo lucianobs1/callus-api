f8c70a68ef9fe65cbf7c83f8be5c82302ec78e2a
Luciano Brandao <lucianotetsu@gmail.com>
2022-02-04 09:33:21 -0300

    refactored folders useCases

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,10 +1,9 @@
 import { hash } from 'bcrypt';
 import { inject, injectable } from 'tsyringe';
 
+import { User } from '@modules/accounts/infra/typeorm/entities/User';
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { AppError } from '@shared/errors/AppError';
-
-import { User } from '../infra/typeorm/entities/User';
-import { IUsersRepository } from '../repositories/IUsersRepository';
 
 interface IRequest {
   name: string;
