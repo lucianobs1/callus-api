dbae7533a7157de04a665087141a0dc7504da23e
Luciano Brandao <lucianotetsu@gmail.com>
2022-02-03 21:46:29 -0300

    added typeorm and migration users

--- src/modules/accounts/entities/User.ts
+++ src/modules/accounts/entities/User.ts
@@ -1,31 +1,44 @@
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn
+} from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+@Entity('users')
 class User {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   username: string;
+
+  @Column()
   password: string;
+
+  @Column()
   avatar?: string;
+
+  @Column()
   is_admin?: boolean;
+
+  @Column()
   birth_date: Date;
+
+  @CreateDateColumn()
   created_at?: Date;
+
+  @UpdateDateColumn()
   updated_at?: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
-    }
-    if (!this.is_admin) {
-      this.is_admin = false;
-    }
-    if (!this.created_at) {
-      this.created_at = new Date();
-    }
-    if (!this.updated_at) {
-      this.updated_at = new Date();
-    }
-    if (!this.avatar) {
-      this.avatar = null;
     }
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
+import 'reflect-metadata';
 import express from 'express';
 import { routes } from 'routes';
+
+import '@shared/infra/typeorm';
 
 const server = express();
 
@@ -7,4 +10,6 @@ server.use(express.json());
 
 server.use(routes);
 
-server.listen(3333, () => console.log('✨ Server is running ✨'));
+server.listen(3333, () => {
+  console.log('✨ Server is running ✨');
+});
